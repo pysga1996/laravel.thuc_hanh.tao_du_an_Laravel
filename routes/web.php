2b1309463060755a2af2456808fb99a5af58d930
vutatthanh.hl96 <vutatthanh.hl96@gmail.com>
2020-03-25 21:07:09 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,39 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
+    use \Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get( '/xin-chao' , function(Request $request){
+        $ten = $request->query('ten');
+        $namsinh = $request->query('namsinh');
+        return 'Chào bạn: ' . $ten.'<br>Có năm sinh là: '.$namsinh;
+    });
+
+    Route::group(['prefix'=>'group'], function () {
+        route::get('1',function () {return "Bạn đang ở nhom/nhom1";});
+        route::get('2',function () {return "Bạn đang ở nhom/nhom2";});
+        route::get('3',function () {return "Bạn đang ở nhom/nhom3";});
+        route::get('4',function () {return "Bạn đang ở nhom/nhom4";});
+    });
+
+    Route::prefix('group')->group(function () {
+        route::get('5',function () {return "Bạn đang ở nhom/nhom5";});
+        route::get('6',function () {return "Bạn đang ở nhom/nhom6";});
+        route::get('7',function () {return "Bạn đang ở nhom/nhom7";});
+        route::get('8',function () {return "Bạn đang ở nhom/nhom8";});
+    });
+
